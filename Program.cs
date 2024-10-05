5825053e10e948496b45429232fd332be93c4974
Wachira Norasing <wachira_norasing@outlook.com>
2024-10-05 18:58:58 +0700

    update db

--- Program.cs
+++ Program.cs
@@ -41,12 +41,12 @@ var app = builder.Build();
 
 app.UseCors("AllowAll");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+// }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
